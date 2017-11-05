e0ea633cbc84dafa834bd83f5bcfb0c378ff0fb4
DESKTOP-DLIDV5K\Calvin Lim <calvinlim42@gmail.com>
2017-11-05 11:26:05 +0800

    -Updated character's script -added object pooling -updated animation

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     [Range(0.0f, 10.0f)] public float _movementSpeed;
-    public float _jumpStrength;
+    public float _gapPerLevel;
     public int _energy;
     
     public LayerMask _whatIsGround;
     public Animator _bottomPartAnimator;
     public Transform _groundCheck;
     public Transform _pushCheck;
-    
+
+    [HideInInspector] public int _currentLevel = 0;
     [HideInInspector] public bool _playerCanMove = true;
-    [HideInInspector]  public bool _timeFreezeActivation = false;
+    [HideInInspector] public bool _timeFreezeActivation = false;
     
     public AudioClip _pickEnergySFX;
     public AudioClip _deathSFX;
@@ -22,6 +23,7 @@ public class Character : MonoBehaviour
 
     //-------------------------------------------------------
     Transform _transform;
+    Vector3 _defaultPosition;
     Rigidbody2D _rigidbody;
     Animator _topPartAnimator;
     AudioSource _audio;
@@ -32,12 +34,13 @@ public class Character : MonoBehaviour
     bool _isGrounded = false;
     bool _isPushed = false;
     bool _isRunning = false;
-    bool _canJump = false;
+    bool _isDefend = false;
     
 
     void Awake()
     {
         _transform = GetComponent<Transform>();
+        _defaultPosition = _transform.position;
         _rigidbody = GetComponent<Rigidbody2D>();
         _topPartAnimator = GetComponent<Animator>();
         _audio = GetComponent<AudioSource>();
@@ -48,40 +51,61 @@ public class Character : MonoBehaviour
         if (!_playerCanMove || Time.timeScale == 0.0f)
             return;
         
-        if (!_isPushed)
-            _rigidbody.velocity = new Vector2(_movementSpeed, _rigidbody.velocity.y);
+        //if (!_isPushed)
+        //    _rigidbody.velocity = new Vector2(_movementSpeed, _rigidbody.velocity.y);
 
         _isGrounded = Physics2D.Linecast(_transform.position, _groundCheck.position, _whatIsGround);
         _isPushed = Physics2D.Linecast(_transform.position, _pushCheck.position, _whatIsGround);
-        
-        if (_isGrounded)
-            _canJump = true;
 
-        _topPartAnimator.SetBool("isGrounded", _isGrounded);
-        _bottomPartAnimator.SetBool("isGrounded", _isGrounded);
 
-        _topPartAnimator.SetBool("isPushed", _isPushed);
-        _bottomPartAnimator.SetBool("isPushed", _isPushed);
-
-        if (_isGrounded && Input.GetKeyDown(KeyCode.Space)/* swipe up */ )
-        {
-            DoJump();
-            _canJump = false;
-        }
-
-        if(Input.GetKeyDown(KeyCode.F1) /*press the time freeze button*/)
+        //------------------------INPUT---------------------------
+        if (Input.GetKeyDown(KeyCode.F1) /*press the time freeze button*/)
         {
             _timeFreezeActivation = !_timeFreezeActivation;
             StartTimeFreeze();
         }
+
+        if(Input.GetKeyDown(KeyCode.F2) && !_isDefend)
+        {
+            DoDefend();
+        }
+
+        if(Input.GetKeyDown(KeyCode.DownArrow)) // swipe up
+        {
+            _currentLevel--;
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow)) // swipe down
+        {
+            _currentLevel++;
+        }
+        _currentLevel = Mathf.Clamp(_currentLevel, 0, 2);
+
+
+        if(_currentLevel == 0)
+        {
+            transform.position = Vector2.MoveTowards(_transform.position, new Vector2(_defaultPosition.x, _defaultPosition.y), _movementSpeed * Time.deltaTime);
+            _rigidbody.gravityScale = 2;
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(_defaultPosition.x, _defaultPosition.y + (_gapPerLevel * _currentLevel)), _movementSpeed * Time.deltaTime);
+            _rigidbody.gravityScale = 0;
+        }
+
+        //----------------------ANIMATION-------------------------
+        _topPartAnimator.SetBool("isGrounded", _isGrounded);
+        _bottomPartAnimator.gameObject.SetActive(_isGrounded);
+
+        _topPartAnimator.SetBool("isDefend", _isDefend);
+        //_bottomPartAnimator.gameObject.GetComponent<SpriteRenderer>().enabled = !_isDefend;
     }
+    
 
-    void DoJump()
+    void DoDefend()
     {
-        _vy = 0f;
-        _rigidbody.AddForce(new Vector2(0, _jumpStrength));
-
-        // play the sound
+        _isDefend = true;
+        StartCoroutine("DefendRefresh");
     }
 
     public void StartTimeFreeze()
@@ -130,5 +154,11 @@ public class Character : MonoBehaviour
 
             //reset game or set to check point
         }
+    }
+    
+    IEnumerator DefendRefresh()
+    {
+        yield return new WaitForSeconds(0.2f);
+        _isDefend = false;
     }
 }
